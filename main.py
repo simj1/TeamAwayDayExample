18d9a2aaeb092d0dd352c84f560a798f2bdb3400
simj1 <44676171+simj1@users.noreply.github.com>
2024-09-10 13:56:22 +0000

    first commit to the example repository

---
+++ main.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot as plt
+import seaborn as sns
+
+
+def main():
+  print("This is a demonstration of using git to manage workflows")
+
+
+if __name__ == "__main__":
+  main()
